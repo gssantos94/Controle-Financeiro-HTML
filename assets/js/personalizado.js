e3a8f0c7c9c84610145a43100fd0ba4e242e7e08
gssantos94 <gssantos@id.uff.br>
2021-06-26 17:54:07 -0300

    :package: pagina inicial pronta

--- assets/js/personalizado.js
+++ assets/js/personalizado.js
@@ -1,3 +1,39 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
   document.querySelector(".caixa").classList.toggle("show-menu")
 );
+
+// Selecione todos os links com hashes
+$('a[href*="#"]')
+  // Remova os links que na verdade não direcionam a nada
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // Links na página
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      // Descobrir o elemento para rolar para
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Existe um alvo de rolagem?
+      if (target.length) {
+        // Apenas previna o padrão se a animação realmente acontecer
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Retorno de chamada após animação
+          // Deve mudar o foco!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Verificar se o alvo estava focado
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adicionando tabindex para elementos não focalizáveis
+            $target.focus(); // Defina o foco novamente
+          };
+        });
+      }
+    }
+  });
